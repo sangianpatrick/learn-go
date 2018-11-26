69aae42da0a25df662d7c420adee9167f99d9203
Patrick Maurits Sangian <patricksangian@gmail.com>
2018-11-27 02:59:46 +0700

    add suffle function to cards instance

--- deck.go
+++ deck.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
 )
 
 // Create a new type of 'deck'
@@ -11,14 +16,19 @@ type deck []string
 func newDeck() deck {
 	cards := deck{}
 
-	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs", "Black Joker", "Red Joker"}
 
-	cardValues := []string{"Ace", "Two", "Three", "Four"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
 
 	for _, suit := range cardSuits {
-		for _, value := range cardValues {
-			cards = append(cards, suit+" of "+value)
+		if suit == "Black Joker" || suit == "Red Joker" {
+			cards = append(cards, suit)
+		} else {
+			for _, value := range cardValues {
+				cards = append(cards, value+" of "+suit)
+			}
 		}
+
 	}
 
 	return cards
@@ -27,5 +37,41 @@ func newDeck() deck {
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
+	}
+}
+
+func deal(d deck, handsize int) (deck, deck) {
+	return d[:handsize], d[handsize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0775)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		// Option #1 - log the error and return a call to newDeck
+		// Option #2 - log the error and entirely quit the program
+
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+		d[i], d[newPosition] = d[newPosition], d[i]
 	}
 }
--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
 
 func main() {
 	// var card string = "Ace of Spades"
-	cards := newDeck()
+	// cards := newDeck()
+	// hand, remainingCards := deal(cards, 7)
+	// hand.print()
+	// remainingCards.print()
 
+	// greeting := "Hi there!"
+	// fmt.Println([]byte(greeting))
+
+	// cards := newDeck()
+	// cards.saveToFile("my_cards.txt")
+
+	cards := newDeckFromFile("my_cards.txt")
+	cards.shuffle()
 	cards.print()
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
